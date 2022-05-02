08f7abb801341da0fbe1829c911c763a88af872c
maxmv <maxmvaas@gmail.com>
2022-05-02 18:35:36 +0400

    add ui for empty notes list

--- app/src/main/java/ru/maxmv/notes/presentation/main/MainActivity.kt
+++ app/src/main/java/ru/maxmv/notes/presentation/main/MainActivity.kt
@@ -1,7 +1,9 @@
-package ru.maxmv.notes
+package ru.maxmv.notes.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import ru.maxmv.notes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/ru/maxmv/notes/presentation/notes_list/NotesListFragment.kt
@@ -0,0 +1,6 @@
+package ru.maxmv.notes.presentation.notes_list
+
+import androidx.fragment.app.Fragment
+import ru.maxmv.notes.R
+
+class NotesListFragment : Fragment(R.layout.fragment_notes_list)
